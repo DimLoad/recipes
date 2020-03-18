c49a97918f0cc04dea38682c7622e29c384eb476
DimLoad <matliviu89@yahoo.com>
2020-03-18 20:09:59 +0200

    added services to main components

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,34 @@
+import { Recipe } from './recipe.model';
+import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Tasy Schnitzel', 
+      'A super-tasy Schnitzel - just awesome!', 
+      'https://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-5.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20),
+        new Ingredient('Eggs', 2)
+      ]
+    ),
+    new Recipe(
+      'Big Fat Burger', 
+      'What else you need to say?', 
+      'https://media.kaufland.com/images/PPIM/AP_Content_2708/std.lang.all/66/67/Asset_3306667.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1),
+        new Ingredient('Onions', 1)
+      ]
+    )
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+} 
